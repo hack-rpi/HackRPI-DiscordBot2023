e21dd880861326448bbba95bae28d8a7cd490d7a
marinocj <35199762+marinocj@users.noreply.github.com>
2023-09-22 17:59:30 -0400

    Update bot_captcha.py

--- bot_captcha.py
+++ bot_captcha.py
@@ -35,4 +35,32 @@ async def send_captcha(member, channel):
     captchas[member.id] = data
 
    
+   
+    file = discord.File(fp=image_data, filename="captcha.png")
+    embed = discord.Embed(title="Verification", description="Solve the CAPTCHA to get verified!")
+    message = await channel.send(embed=embed, file=file, components=[[
+        discord.ui.Button(style=discord.ButtonStyle.green, label="I'm Human", custom_id="verify_me")
+    ]])
+
+   
+    try:
+        interaction = await bot.wait_for("button_click", check=lambda i: i.custom_id == "verify_me" and i.user == member, timeout=120)
+        await interaction.response.send_message("Please type the CAPTCHA content to verify!", ephemeral=True)
+    except TimeoutError:
+        await message.edit(content="CAPTCHA verification time expired. Please request a new CAPTCHA if you wish to verify.", components=[])
+
+@bot.event
+async def on_message(message):
+    if message.author.bot:
+        return
     
+    if message.author.id in captchas:
+        if message.content == captchas[message.author.id]:
+            await message.channel.send(f"Welcome {message.author.mention}, you are now verified!")
+            role = discord.utils.get(message.guild.roles, name="Verified")
+            await message.author.add_roles(role)
+            del captchas[message.author.id]
+        else:
+            await message.channel.send(f"{message.author.mention}, that's incorrect. Please try again.")
+
+bot.run(token)
