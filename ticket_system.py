a5b93ff7de33eaf077c7204a9005120415a36f40
iamdanielyun <dsy3712@gmail.com>
2023-09-26 17:10:37 -0400

    Queue and dequeue working
    Put teams into queue upon command and dequeue upon command; also connected with discord bot

--- ticket_system.py
+++ ticket_system.py
@@ -3,17 +3,18 @@ from ticket import Ticket
 from ticket import create_ticket as create
 import discord
 
-#Discord token
-token = config("DISCORD_BOT_TOKEN")
-print(token)
 #Connection
 intents = discord.Intents.default()
 intents.typing = False
 intents.presences = False
+intents.message_content = True
 token = config("DISCORD_BOT_TOKEN")
 
 # Initialize the bot with the specified intents
 bot = discord.Client(intents=intents)
+
+#Queue of all teams
+queue = []
 
 #Bot is ready
 @bot.event
@@ -25,32 +26,61 @@ async def on_ready():
 async def on_message(message):
 
     author = message.author
-
+    content = message.content
+   
     #Make sure the message isn't from us
     if author == bot.user:
         return
 
-    #If message starts with 
-    content = message.content
-    if content.startswith('!'):
-        await message.channel.send('A mentor will be with you shortly!')
-
+    #If help message
+    if content[0] == '!':
         #Starts at index 1 because index 0 is '!' 
         # which we need for the bot to recognize it as a help message
         reason = content[1:]   
 
-        #Create ticket
-        ticket = create(author, reason)
+        #Put into queue
+        put_into_queue(queue, author, reason)
+
+        await message.channel.send('Current queue: ')
+        for team in queue:
+            return_msg = str(team.name) + ": " + str(team.reason)
+            await message.channel.send(return_msg)
         
-#Find ticket by team name and reason
-def find_ticket(list, team_name, reason):
+    #If resolve message
+    elif content[0] == '?':
+        
+        #Team to remove from queue
+        name = content[1:]
+        ticket = find_ticket(queue, name)
+        queue.remove(ticket)
+
+        await message.channel.send('Removed ' + name)
+        await message.channel.send('Modified queue: ')
+        
+        if len(queue) == 0:
+            await message.channel.send("Empty queue")
+        else:
+            for team in queue:
+                return_msg = str(team.name) + ": " + str(team.reason)
+                await message.channel.send(return_msg)
+
+    #print queue
+    elif content == "print":
+        if len(queue) == 0:
+            await message.channel.send("Empty queue")
+        for team in queue:
+            return_msg = str(team.name) + ": " + str(team.reason)
+            await message.channel.send(return_msg)
+
+#Find ticket by team name  
+def find_ticket(list, team_name):
 
     for ticket in list:
-        if ticket.name == team_name and ticket.reason == reason:
+        if str(ticket.name) == team_name:
             return ticket
     
-    #if no ticket found, create and return
-    return create(team_name, reason)
+    #If no ticket found
+    return "Something went wrong"
 
 #Put team that needs help in queue
 def put_into_queue(list, team_name, reason):
