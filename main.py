9e3349ef3b03c53b73bda8da64a572f964ac1034
darrenW04 <darrenwong1000@gmail.com>
2023-09-26 17:05:51 -0400

    Working input comparisons

--- main.py
+++ main.py
@@ -1,28 +1,56 @@
 from decouple import config
 import discord
-
-def check_profanity(sentence):
-	if():
-		return True
-	return False
+from discord.ext import commands
 
 
-token = config("DISCORD_BOT_TOKEN")
+async def check_profanity(sentence):
+    word_list = ["apple", "banana", "cherry", "date", "elderberry", "fig", "grape"]
+    for word in word_list:
+        if(sentence == word):
+        # If a word from the list is found, send a response
+            return word
 
 
-bot = discord.Client()
+if __name__ =='__main__':
+    # Define your intents
+    intents = discord.Intents.default()
+    intents.typing = False
+    intents.presences = False
+    intents.messages = True
+    intents.message_content = True
+    token = config("DISCORD_BOT_TOKEN")
 
 
-@bot.event
-async def on_message(message):
-	word_list = ["apple", "banana", "cherry", "date", "elderberry", "fig", "grape"]
+    # Initialize the bot with the specified intents
+    bot = discord.Client(intents=intents)
 
-	# CHECKS IF THE MESSAGE THAT WAS SENT IS EQUAL TO "HELLO".
-	if message.content.lower() == "hello":
-		# SENDS BACK A MESSAGE TO THE CHANNEL.
-		await message.channel.send("hey testing 123")
-          
 
-        
+    @bot.event
+    async def on_ready():
+        print(f'Logged in as {bot.user.name}')
 
-bot.run(token)
+    @bot.event
+    async def on_message(message):
+        word_list = ["apple", "banana", "cherry"]
+
+        # Convert the message content to lowercase for case-insensitive matching
+        content = message.content.lower().strip()
+
+        # Check if any word in the list is in the message content
+        profanity = ""
+        if message.author == bot.user:
+            return
+        # for word in word_list:
+        #     # await message.channel.send(word)
+        #     if(str(content) == word):
+        #     # If a word from the list is found, send a response
+        #         profanity = word
+
+        profanity = check_profanity(content)
+        if(profanity != "") : 
+            await message.channel.send("Word isnt allowed, "+ profanity)
+        else:
+            await message.channel.send("Clean message" + profanity)
+
+ 
+    bot.run(token)
