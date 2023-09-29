73b3aa76abf6139e8cff8b9287f9bead36b81d7e
CalebRD <69176829+CalebRD@users.noreply.github.com>
2023-09-29 16:44:55 -0400

    Basic Functionality
    Allows a user to assign themselves and 3 other people to a team role.

--- automatically_assign_roles.py
+++ automatically_assign_roles.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 import os
 from dotenv import load_dotenv
 from decouple import config
+    
 
 
 # Load environment variables from a .env file
@@ -21,8 +22,21 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 
 # Define a simple command
 @bot.command()
-async def test(ctx):
-    await ctx.send('Congrat\'s bud, it worked :)')
+async def team(ctx, name, member1: discord.Member, member2: discord.Member, member3: discord.Member):
+    await ctx.send('Team Created with name ' + name)
+    role = await ctx.guild.create_role(name=name)
+
+#assign roles to the author and users
+    user = ctx.author
+
+    role = discord.utils.get(ctx.guild.roles, name=name)
+
+
+    await user.add_roles(role)
+    await member1.add_roles(role)
+    await member2.add_roles(role)
+    await member3.add_roles(role)
+
 
 # Event handler for when the bot is ready
 @bot.event
