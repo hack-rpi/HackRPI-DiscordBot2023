38d26c3d913a5cc74a7b4f7753744c38085bb1d9
alin600 <alilin.al6@gmail.com>
2023-10-03 17:21:43 -0400

    spam of 5 for 10 minute timeout

--- spam.py
+++ spam.py
@@ -1,5 +1,6 @@
 from decouple import config
 import discord
+import asyncio
 from discord.ext import commands
 
 token = config("DISCORD_BOT_TOKEN")
@@ -19,4 +20,27 @@ async def on_message(message):
     if message.author == bot.user:
         return
     
-    
+
+    author_id = str(message.author.id)
+    if author_id in spam_count:
+        spam_count[author_id] += 1
+    else:
+        spam_count[author_id] = 1
+
+
+    if spam_count[author_id] > 5:
+        await message.author.send("You are spamming. Please stop.")
+        await message.author.add_roles(discord.utils.get(message.author.guild.roles, name="Timeout"))
+        await message.author.send("You have been timed out for 10 minutes.")
+        await message.delete()
+        await asyncio.sleep(600) 
+        await message.author.remove_roles(discord.utils.get(message.author.guild.roles, name="Timeout"))
+        del spam_count[author_id]
+
+    await bot.process_commands(message)
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send('Pong!')
+
+bot.run(token)
