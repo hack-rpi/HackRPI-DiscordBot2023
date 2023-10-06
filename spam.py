6696f31a66e18fbaa5769aa12a50d5c78414da9e
alin600 <alilin.al6@gmail.com>
2023-10-06 19:00:24 -0400

    attempt to stop messages during timeout

--- spam.py
+++ spam.py
@@ -29,24 +29,38 @@ async def on_message(message):
     author_id = str(message.author.id)
 
     if author_id not in last_messages:
-        last_messages[author_id] = deque(maxlen=5)
+        last_messages[author_id] = deque(maxlen=3)
 
     last_messages[author_id].append(time.time())
 
-    if len(last_messages[author_id]) == 5 and (time.time() - last_messages[author_id][0]) <= 10:
+    if len(last_messages[author_id]) == 3 and (time.time() - last_messages[author_id][0]) <= 5:
         if author_id in spam_count:
             spam_count[author_id] += 1
         else:
             spam_count[author_id] = 1
 
-        if spam_count[author_id] > 5:
+        if spam_count[author_id] > 3:
             await message.author.send("You are spamming. Please stop.")
-            await message.author.add_roles(discord.utils.get(message.author.guild.roles, name="Timeout"))
-            await message.author.send("You have been timed out for 10 minutes.")
+            timeout_role = discord.utils.get(message.author.guild.roles, name="Timeout")
+            await message.author.add_roles(timeout_role)
+
+            original_roles = message.author.roles.copy()
+
+            for role in original_roles:
+                if role != timeout_role:
+                    print(role)
+                    await message.author.remove_roles(role)
+
+            await message.author.send("You have been placed in timeout for 2 minutes.")
             await message.delete()
-            await asyncio.sleep(600) 
-            await message.author.remove_roles(discord.utils.get(message.author.guild.roles, name="Timeout"))
-            del spam_count[author_id]
+            await asyncio.sleep(120) 
+
+
+            await message.author.remove_roles(timeout_role)
+            for role in original_roles:
+                await message.author.add_roles(role)
+
+            spam_count.pop(author_id, None)
 
     await bot.process_commands(message)
 
