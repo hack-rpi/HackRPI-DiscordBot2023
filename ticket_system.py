d3379bede8a0d6e797c50776f51f129f77b27327
iamdanielyun <dsy3712@gmail.com>
2023-10-06 17:13:41 -0400

    Messages now to dms not entire channel

--- ticket_system.py
+++ ticket_system.py
@@ -38,11 +38,24 @@ async def print_queue(queue, channel):
             return_msg = str(team.name) + ": " + str(team.reason)
             await print_msg(return_msg, channel)
 
+"""
+user = message.author
+dm_channel = await user.create_dm()
+await dm_channel.send(msg)
+"""
+
 #Print msg
 @bot.command()
 async def print_msg(msg, channel):
     await channel.send(msg)
     
+#dm msg
+@bot.command()
+async def dm(msg, author):
+    dm_channel = await author.create_dm()
+    await dm_channel.send(msg)
+
+
 #Bot receives help message
 @bot.event
 async def on_message(message):
@@ -61,9 +74,19 @@ async def on_message(message):
 
     #Instructions
     if content.startswith("!mentorhelp"):
+        msg = """
+                Hi! Please keep the assistance requests in the following format: 
+                !mentors [Table #] [Programming Languages used] [Short Description] 
+                WARNING: If you do not follow this format, we will not get to you immediately"""
+
+        await dm(msg, author)
+
+        """
         await print_msg('Hi! Please keep the assistance requests in the following format:\n' + 
         '!mentors [Table #] [Programming Languages used] [Short Description]\n'
         'WARNING: If you do not follow this format, we will not get to you immediately', channel)
+        """
+        
 
     #If help message
     elif content.startswith('!mentors'):
@@ -75,47 +98,86 @@ async def on_message(message):
         if ticket != None:
             resolve(queue, author)
 
+        #How many ppl ahead of me?
+        ppl_ahead_of_me = len(queue)
+
         #Put into queue
         put_into_queue(queue, author, reason)
-        await print_queue(queue, channel)
+        msg = "Currently " + str(ppl_ahead_of_me) + " people ahead"
+
+        await dm(msg, author)
+        #await print_msg("Currently " + str(ppl_ahead_of_me) + " people ahead", channel)
          
     #If resolve message
     elif content[0] == '?':
+
+        #ONLY AUTHORIZED CAN DO THIS
+        """
+        if not authorized(author):
+            await print_msg("Not authorized", channel)
+            return
+        """
+        if not authorized(author):
+            await dm("Not authorized", author)
+            return
 
         #Team to remove from queue
         name = content[1:]
         ticket = find_ticket(queue, name)
 
+        """
         if ticket is None:
             await print_msg("Unknown team", channel)
             return 
-
-        #ONLY MENTORS CAN DO THIS
-        if not authorized(author):
-            await print_msg("Not authorized", channel)
-            return
+        """
+        if ticket is None:
+            await dm("Unknown team", author)
+            return 
 
         resolve(queue, name)
-        await print_msg('Removed ' + name, channel)
-        await print_msg('Modified queue: ', channel)
-        
+        # await print_msg('Removed ' + name, channel)
+        await dm('Removed ' + name, author)
+
+        """
         if len(queue) == 0:
+            await print_msg('Modified queue: ', channel)
             await print_msg("Empty queue", channel)
         else:
             await print_queue(queue, channel)
+        """
+        if len(queue) == 0:
+            await dm('Modified queue: ', author)
+            await dm("Empty queue", author)
+        else:
+            await dm(queue, author)
 
     #print queue
     elif content.startswith('!q'):
 
+        """
         if not authorized(author):
             await print_msg("Not authorized", channel)
             return
         await print_queue(queue, channel)
+        """
+        if not authorized(author):
+            await dm("Not authorized", author)
+            return
+        #await print_queue(queue, channel)
+
+        if len(queue) == 0:
+            await dm("Empty queue", author)
+        else:
+            await dm('Current queue: ', author)
+            for team in queue:
+                return_msg = str(team.name) + ": " + str(team.reason)
+                await dm(return_msg, author)
 
     #Unknown command
     elif content.startswith('!') or content.startswith('?'):
-        await print_msg('Command not found. Type !mentorhelp for commands', channel)
-
+        #await print_msg('Command not found. Type !mentorhelp for commands', channel)
+        await dm('Command not found. Type !mentorhelp for commands', author)
+        
 #Find ticket by team name  
 def find_ticket(list, team_name):
 
