553bf63aacb4d7401b9fd69a95a2c5c6cd645590
LuCaS9798 <83709408+LuCaS9798@users.noreply.github.com>
2023-10-06 17:10:37 -0400

    Update temp.py

--- temp.py
+++ temp.py
@@ -14,30 +14,33 @@ async def on_ready():
 
 @bot.command()
 async def create_teams(ctx, num_players: int):
-    # Get the server
-    server = ctx.guild
+    if ctx.author.guild_permissions.administrator:
+        # Get the server
+        server = ctx.guild
 
-    # Get the list of members
-    members = server.members
+        # Get the list of members
+        members = server.members
 
-    # Calculate the number of teams
-    num_teams = len(members) // num_players
+        # Calculate the number of teams
+        num_teams = len(members) // num_players
 
-    # Create teams
-    for i in range(num_teams):
-        # Create a text channel
-        text_channel = await server.create_text_channel(f'team-{i + 1}')
+        # Create teams
+        for i in range(num_teams):
+            # Create a text channel
+            text_channel = await server.create_text_channel(f'team-{i + 1}')
 
-        # Create a voice channel
-        voice_channel = await server.create_voice_channel(f'team-{i + 1}')
+            # Create a voice channel
+            voice_channel = await server.create_voice_channel(f'team-{i + 1}')
 
-        # Move members to the team
-        for j in range(num_players):
-            member = members.pop()
-            await member.move_to(voice_channel)
+            # Move members to the team
+            for j in range(num_players):
+                member = members.pop()
+                await member.move_to(text_channel)
+                await member.move_to(voice_channel)
 
-        await text_channel.send(f'Team-{i + 1} has been created, and members have been assigned.')
-
+            await text_channel.send(f'Team-{i + 1} has been created, and members have been assigned.')
+    else:
+        print('Sorry you have not right to create channels.')
     await ctx.send(f'Successfully created {num_teams} teams!')
 
 
