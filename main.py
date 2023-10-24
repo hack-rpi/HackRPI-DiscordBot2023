b44d36367eef45a11b5e61d3bb6c047358553740
darrenW04 <darrenwong1000@gmail.com>
2023-10-24 16:14:15 -0400

    merging CJ's code and fix conflicts

--- main.py
+++ main.py
@@ -2,7 +2,12 @@ from decouple import config
 import discord
 from discord.ext import commands
 from datetime import timedelta
-
+from captcha.image import ImageCaptcha
+import random
+import io
+import os
+from PIL import Image
+import asyncio
 
 def check_profanity(sentence):
     bad_words = set()
@@ -18,6 +23,8 @@ def check_profanity(sentence):
     sentence_set = set(words)
     return sentence_set.intersection(bad_words)
 
+
+
 if __name__ =='__main__':
     # Define your intents
     intents = discord.Intents.default()
@@ -26,6 +33,61 @@ if __name__ =='__main__':
     intents.messages = True
     intents.message_content = True
     token = config("DISCORD_BOT_TOKEN")
+    captcha_generator = ImageCaptcha()
+
+    captchas ={}
+
+    async def send_captcha(member):
+        while True:  # Continues until correct CAPTCHA or timeout
+            data = ''.join(random.choices('ABCDEFGHJKMNPQRSTUVWXYZ123456789', k=5))
+            captcha_image = captcha_generator.generate(data)
+            captcha_bytes = captcha_image.getvalue()
+
+            temp_file = f"captcha_{member.id}.png"
+            with open(temp_file, "wb") as f:
+                f.write(captcha_bytes)
+
+            captchas[member.id] = data
+
+            dm_channel = await member.create_dm()
+
+            image = Image.open(temp_file)
+            image.close()
+
+            file = discord.File(fp=temp_file, filename="captcha.png")
+
+            embed = discord.Embed(title="Verification", description="Solve the CAPTCHA to get verified!")
+            await dm_channel.send(embed=embed, file=file)
+
+            fail_count = 0
+            timeout = 180 
+            start_time = asyncio.get_event_loop().time()
+
+            while (asyncio.get_event_loop().time() - start_time) < timeout:
+                try:
+                    response = await bot.wait_for('message', check=lambda m: m.author == member and isinstance(m.channel, discord.DMChannel), timeout=timeout)
+
+                    if response.content == captchas[member.id]:
+                        role = discord.utils.get(member.guild.roles, name="Verified")
+                        await member.add_roles(role)
+                        await dm_channel.send(f"Welcome {member.mention}, you are now verified and have been given the 'Verified' role!")
+                        del captchas[member.id]
+                        return
+                    else:
+                        fail_count += 1
+                        if fail_count < 3:
+                            await dm_channel.send(f"Sorry {member.mention}, that's incorrect. You have {3 - fail_count} attempts left. A new captcha will be sent after your remaining attempts run out.")
+                        else:
+                            await dm_channel.send(f"Sorry {member.mention}, that's incorrect. Generating a new CAPTCHA for you...")
+                            break  # Exit the inner loop to generate a new CAPTCHA
+
+                except asyncio.TimeoutError:
+                    await dm_channel.send(f"Verification timed out. Please request a new CAPTCHA if you wish to verify.")
+                    return
+
+            os.remove(temp_file)
+
+
 
     # Initialize the bot with the specified intents
     bot = discord.Client(intents=intents)
@@ -48,6 +110,10 @@ if __name__ =='__main__':
         # Check if any word in the list is in the message content
         if message.author == bot.user:
             return
+            
+        if message.guild is None:
+            return
+        
         if content.startswith("!"):
             return
         profanity = check_profanity(content)
@@ -68,8 +134,21 @@ if __name__ =='__main__':
                 "You have received a warning for this violation. This warning is a reminder of our rules and a request to adhere to them. Repeated violations may lead to further actions, such as muting or removal from the server.\n"
                 "If you have any questions or concerns, feel free to reach out to the server moderators or administrators. We encourage positive and respectful interactions among our members."
             )
+        if message.author.id in captchas:
+            if message.content == captchas[message.author.id]:
+                await message.author.send(f"Welcome {message.author.mention}, you are now verified!")
+                role = discord.utils.get(message.guild.roles, name="Verified")
+                await message.author.add_roles(role)
+                del captchas[message.author.id]
+            else:
+                await message.author.send(f"{message.author.mention}, that's incorrect. Please try again.")
 
 
+    @bot.event
+    async def on_member_join(member):
+        await send_captcha(member)
+    
+        
     bot.run(token)
 
 
