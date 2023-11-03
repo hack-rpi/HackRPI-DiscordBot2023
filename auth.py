1f6158fb548f74bd0c9401b852af13e2e118dab9
iamdanielyun <dsy3712@gmail.com>
2023-11-03 16:52:36 -0400

    spelling

--- auth.py
+++ auth.py
@@ -1,7 +1,7 @@
 
 #Auth check for certain commands
 def authorized(author):
-    authorized_individuals = ["Director", "President", "Vice President", "Jr. Director", "Mentors"]
+    authorized_individuals = ["Director", "President", "Vice-President", "Jr. Director", "Mentors"]
 
     for role in authorized_individuals:
         if role.lower() in [y.name.lower() for y in author.roles]:
