881948e699f18d296341c0f14df137c3587743c4
iamdanielyun <dsy3712@gmail.com>
2023-11-17 17:07:34 -0500

    Update task_list.py

--- task_list.py
+++ task_list.py
@@ -6,7 +6,7 @@ from discord.ext import commands
 from discord.ext.commands import has_permissions, MissingPermissions
 import discord
 
-#Connection
+# Connection
 intents = discord.Intents.default()
 intents.typing = False
 intents.presences = False
@@ -15,81 +15,62 @@ token = config("DISCORD_BOT_TOKEN")
 
 bot = commands.Bot(command_prefix='!', intents=intents)
 
-#List of all tasks
+# List of all tasks
 tasks = TaskList()
 
-#Initialize server
+# Initialize server
 @bot.event
 async def on_ready():
-    print("HELLO!")
+    print("Bot is online and ready to manage tasks!")
 
-#Helper function to print all tasks
+# Helper function to print all tasks
 @bot.command()
-async def print_tasks(channel):
-
-    msg = ""
-    #Iterate through all tasks
+async def print_tasks(ctx):
+    msg = "Current Tasks:\n"
     for task in tasks:
         task_description = task.task
         team_in_charge = task.team_in_charge
         team_to_display = "Unclaimed"
-
-        if  team_in_charge is not None:
+        if team_in_charge is not None:
             team_to_display = team_in_charge
+        msg += f"Team: {team_to_display} ---- Task: {task_description}\n"
+    await ctx.send(msg)
 
-        msg += team_to_display + "  ----   " + task_description + "\n"
-
-    await channel.send(msg)
-
-#Bot receivees message
+# Bot receives a message
 @bot.event
 async def on_message(message):
-
     author = message.author
     content = message.content
     channel = message.channel
 
-    #If create task
     if content.startswith("$newtask"):
         description = str(content[9:])
-
-        #Creates new task and adds it to list
         create_task(tasks, description)
 
-    #If "claiming a task"
     if content.startswith("$claimtask"):
         description = str(content[11:])
-
-        #Assign task with author
         task = tasks.find(description)
         msg = ""
-
         if task is None:
             msg = "Unknown task"
         else:
             task.assign_team(author)
-            msg = "Task claimed by " + str(author)
-
+            msg = f"Task '{description}' claimed by {author}"
         await channel.send(msg)
 
-    #If view all tasks
     if content.startswith("$alltasks"):
-        print_tasks(channel)
+        await print_tasks(channel)
 
-    #If resolve a task
     if content.startswith("$resolvetask"):
         description = str(content[13:])
-
-        #Find this task
         task = tasks.find(description)
-        
-        #If not found
         if task is None:
             msg = "Unknown task"
         else:
             task.completed()
-            msg = "Completed task " + description
-
+            msg = f"Completed task '{description}'"
         await channel.send(msg)
+
+    await bot.process_commands(message)  # Ensure commands are processed
 
 bot.run(token)
