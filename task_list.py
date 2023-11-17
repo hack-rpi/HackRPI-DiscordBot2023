5c41d94d5e8fc6ec60d893989f81329f8b2cb911
iamdanielyun <dsy3712@gmail.com>
2023-11-17 17:10:09 -0500

    Update task_list.py

--- task_list.py
+++ task_list.py
@@ -1,9 +1,7 @@
 from decouple import config
 from task import Task, TaskList, create_task
 from ticket import create_ticket as create
-from discord import client
 from discord.ext import commands 
-from discord.ext.commands import has_permissions, MissingPermissions
 import discord
 
 # Connection
@@ -15,62 +13,65 @@ token = config("DISCORD_BOT_TOKEN")
 
 bot = commands.Bot(command_prefix='!', intents=intents)
 
-# List of all tasks
 tasks = TaskList()
 
-# Initialize server
 @bot.event
 async def on_ready():
     print("Bot is online and ready to manage tasks!")
 
-# Helper function to print all tasks
 @bot.command()
 async def print_tasks(ctx):
     msg = "Current Tasks:\n"
     for task in tasks:
         task_description = task.task
         team_in_charge = task.team_in_charge
-        team_to_display = "Unclaimed"
-        if team_in_charge is not None:
-            team_to_display = team_in_charge
+        team_to_display = "Unclaimed" if team_in_charge is None else team_in_charge
         msg += f"Team: {team_to_display} ---- Task: {task_description}\n"
     await ctx.send(msg)
 
-# Bot receives a message
+@bot.command()
+async def new_task(ctx, *, description):
+    create_task(tasks, description)
+    await ctx.send(f"New task '{description}' added!")
+
+@bot.command()
+async def claim_task(ctx, *, description):
+    task = tasks.find(description)
+    if task is None:
+        await ctx.send("Unknown task")
+    else:
+        task.assign_team(ctx.author)
+        await ctx.send(f"Task '{description}' claimed by {ctx.author}")
+
+@bot.command()
+async def all_tasks(ctx):
+    await print_tasks(ctx)
+
+@bot.command()
+async def resolve_task(ctx, *, description):
+    task = tasks.find(description)
+    if task is None:
+        await ctx.send("Unknown task")
+    else:
+        task.completed()
+        await ctx.send(f"Completed task '{description}'")
+
+@bot.command()
+async def unclaimed_tasks(ctx):
+    unclaimed = [task.task for task in tasks if task.team_in_charge is None]
+    if not unclaimed:
+        await ctx.send("No unclaimed tasks.")
+    else:
+        msg = "Unclaimed Tasks:\n" + "\n".join(unclaimed)
+        await ctx.send(msg)
+
 @bot.event
-async def on_message(message):
-    author = message.author
-    content = message.content
-    channel = message.channel
-
-    if content.startswith("$newtask"):
-        description = str(content[9:])
-        create_task(tasks, description)
-
-    if content.startswith("$claimtask"):
-        description = str(content[11:])
-        task = tasks.find(description)
-        msg = ""
-        if task is None:
-            msg = "Unknown task"
-        else:
-            task.assign_team(author)
-            msg = f"Task '{description}' claimed by {author}"
-        await channel.send(msg)
-
-    if content.startswith("$alltasks"):
-        await print_tasks(channel)
-
-    if content.startswith("$resolvetask"):
-        description = str(content[13:])
-        task = tasks.find(description)
-        if task is None:
-            msg = "Unknown task"
-        else:
-            task.completed()
-            msg = f"Completed task '{description}'"
-        await channel.send(msg)
-
-    await bot.process_commands(message)  # Ensure commands are processed
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.CommandNotFound):
+        await ctx.send("Invalid command. Use !help to see available commands.")
+    elif isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send("Please provide all necessary arguments for the command.")
+    else:
+        await ctx.send("An error occurred while executing the command.")
 
 bot.run(token)
