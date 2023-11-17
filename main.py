b46af1db05f1715f097ed5c6e86731cd565097b1
darrenW04 <darrenwong1000@gmail.com>
2023-11-17 16:08:15 -0500

    changing methods

--- main.py
+++ main.py
@@ -34,46 +34,39 @@ class TaskQueue:
         self.task_statuses = {}  # A dictionary to store task statuses by task ID
         self.task_assignments = {}  # A dictionary to store task assignments by task ID
 
-    async def add_task(self, task_description):
-        if(task_description.strip() == ""):
-           return (f"Empty task, please enter a task description.")
-        channel_id = ctx.channel.id
-        task_id = len(self.queues[channel_id]) + 1
-        self.queues[channel_id].append(task_id)
-        self.task_statuses[task_id] = "Pending"
-        self.task_assignments[task_id] = None
-        return (f"Task {task_id}: {task_description} has been added to the queue.")
+    def add_task(self, channel_id, task_description):
+            if task_description.strip() == "":
+                return "Empty task, please enter a task description."
+            task_id = len(self.queues[channel_id]) + 1
+            self.queues[channel_id].append(task_id)
+            self.task_statuses[task_id] = "Pending"
+            self.task_assignments[task_id] = None
+            return f"Task {task_id}: {task_description} has been added to the queue."
 
-    async def assign_task(self, ctx, task_id, member: discord.Member):
-        channel_id = ctx.channel.id
-        if(task_id.strip() == ""):
-           await ctx.send(f"Empty task ID, please enter a task ID.")
-           return
+    def assign_task(self, channel_id, task_id, member: discord.Member):
+        task_id = int(task_id)
+        if task_id == "":
+            return "Empty task ID, please enter a task ID."
         if task_id not in self.queues[channel_id]:
-            await ctx.send(f"Task {task_id} does not exist in this channel's queue.")
-            return
+            return f"Task {task_id} does not exist in this channel's queue."
         if self.task_statuses[task_id] != "Pending":
-            await ctx.send(f"Task {task_id} is not pending and cannot be assigned.")
-            return
+            return f"Task {task_id} is not pending and cannot be assigned."
         self.task_assignments[task_id] = member
         self.task_statuses[task_id] = "Assigned"
-        await ctx.send(f"Task {task_id} has been assigned to {member.display_name}.")
+        return f"Task {task_id} has been assigned to {member.display_name}."
 
-    async def complete_task(self, ctx, task_id):
-        channel_id = ctx.channel.id
-        if(task_id.strip() == ""):
-            await ctx.send(f"Empty task ID, please enter a task ID.")
-            return
+    def complete_task(self, channel_id, task_id):
+        task_id = int(task_id)
+        if task_id == "":
+            return "Empty task ID, please enter a task ID."
         if task_id not in self.queues[channel_id]:
-            await ctx.send(f"Task {task_id} does not exist in this channel's queue.")
-            return
+            return f"Task {task_id} does not exist in this channel's queue."
         if self.task_statuses[task_id] != "Assigned":
-            await ctx.send(f"Task {task_id} is not assigned and cannot be marked as complete.")
-            return
+            return f"Task {task_id} is not assigned and cannot be marked as complete."
         assigned_member = self.task_assignments[task_id]
         self.task_statuses[task_id] = "Completed"
         self.task_assignments[task_id] = None
-        await ctx.send(f"Task {task_id} has been marked as completed by {assigned_member.display_name}.")
+        return f"Task {task_id} has been marked as completed by {assigned_member.display_name}."
 
 
 if __name__ =='__main__':
@@ -157,20 +150,24 @@ if __name__ =='__main__':
     async def on_message(message):
         user = message.author
         content = message.content.lower().strip()
-        if(content.startswith("$add_task")):
+        if content.startswith("$add_task"):
             print(content)
-            task_description = content.split(" ")
-            await print(queue.add_task(task_description))
+            task_description = content.split(" ")[1:]
+            output_message = queue.add_task(message.channel.id, " ".join(task_description))
+            await message.channel.send(output_message)
 
-        if(content.startswith("$assign_task")):
-            task_id = content.split(" ")
-            queue.assign_task(task_id)
-        
-        if(content.startswith("$complete_task")):
+        if content.startswith("$assign_task"):
+            task_id = content.split(" ")[1]
+            member = message.author  # Assuming the message sender is the member to assign the task
+            output_message = queue.assign_task(message.channel.id, task_id, member)
+            await message.channel.send(output_message)
 
-            task_id = content.split(" ")
-            queue.complete_task(task_id)
-        
+        if content.startswith("$complete_task"):
+            task_id = content.split(" ")[1]
+            output_message = queue.complete_task(message.channel.id, task_id)
+            await message.channel.send(output_message)
+
+            
     
 
         # Define a list of channel IDs where you want the function to work
